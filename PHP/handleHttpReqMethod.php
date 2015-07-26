45df588d7d25a07ec23ed1c41d44d0f4f98a329b
Shafeen M <sodl02@gmail.com>
2015-07-26 02:29:28 -0400

    updated handleHttpReqMethod.php showing how to grab encoded vars

--- PHP/handleHttpReqMethod.php
+++ PHP/handleHttpReqMethod.php
@@ -10,26 +10,30 @@ function printRequestMethod() {
 switch ($_SERVER["REQUEST_METHOD"]) {
     case "GET":
         printRequestMethod();
+        echo json_encode($_GET);
         // do something ...
         break;
 
     case "POST":
         printRequestMethod();
+        echo json_encode($_POST);
         // do something ...
         break;
 
     case "PUT":
         printRequestMethod();
+        parse_str(file_get_contents("php://input"), $put_vars);
+        echo json_encode($put_vars);
         // do something ...
         break;
 
     case "DELETE":
         printRequestMethod();
+        parse_str(file_get_contents("php://input"), $delete_vars);
+        echo json_encode($delete_vars);
         // do something ...
         break;
 
     default:
         break;
 }
-
-
