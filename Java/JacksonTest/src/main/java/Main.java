c96045201a1e836bc51e8aed3b8bb24fdc429488
Shafeen M <sodl02@gmail.com>
2015-08-15 22:58:41 -0400

    small nonfunctional changes

--- Java/JacksonTest/src/main/java/Main.java
+++ Java/JacksonTest/src/main/java/Main.java
@@ -6,9 +6,9 @@ import java.net.URISyntaxException;
 public class Main {
     public static void main(String[] args) throws IOException, URISyntaxException {
 
-        File f = new File(Main.class.getResource("employee.json").toURI());
+        File file = new File(Main.class.getResource("employee.json").toURI());
         ObjectMapper mapper = new ObjectMapper();
-        Employee e = mapper.readValue(f, Employee.class);
+        Employee e = mapper.readValue(file, Employee.class);
         System.out.println(e);
     }
 
