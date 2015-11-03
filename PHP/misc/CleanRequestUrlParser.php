e491d0e49bfbf2e7b17dbefbaef3cde22370f150
Shafeen M <sodl02@gmail.com>
2015-11-03 14:45:37 -0500

    updated the Utility class CleanRequestUrlParser

--- PHP/misc/CleanRequestUrlParser.php
+++ PHP/misc/CleanRequestUrlParser.php
@@ -36,7 +36,32 @@ class CleanRequestUrlParser
     }
 
     /** @param int $offset */
-    public function parseGetVars($offset) {
-        // TODO: complete this function to set the $_GET superglobal
+    public function parseGetVars($offset=0) {
+        if ($_SERVER["REQUEST_METHOD"] === "GET") {
+            $offset = abs($offset);
+            // set request params in the $_GET superglobal
+            $explodedCleanRequestUrl = $this->getExplodedCleanRequestUrl();
+            $arrayCount = count($explodedCleanRequestUrl);
+            for ($i = $offset; $i < $arrayCount; $i+=2) {
+                if ($i+1 < $arrayCount) {
+                    $_GET[$explodedCleanRequestUrl[$i]]=$explodedCleanRequestUrl[$i+1];
+                }
+            }
+        }
+    }
+
+    /** @param int $offset */
+    public function parsePostVars($offset=0) {
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            $offset = abs($offset);
+            // set request params in the $_POST superglobal
+            $explodedCleanRequestUrl = $this->getExplodedCleanRequestUrl();
+            $arrayCount = count($explodedCleanRequestUrl);
+            for ($i = $offset; $i < $arrayCount; $i+=2) {
+                if ($i+1 < $arrayCount) {
+                    $_POST[$explodedCleanRequestUrl[$i]]=$explodedCleanRequestUrl[$i+1];
+                }
+            }
+        }
     }
 }
