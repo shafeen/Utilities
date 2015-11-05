1577d139728bd927f1d398df6896279dbf0feb5f
Shafeen M <sodl02@gmail.com>
2015-11-05 17:01:56 -0500

    added an abstract BaseModuleController

---
+++ PHP/mvc/controllers/BaseModuleController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace framework\controllers;
+
+
+use framework\components\CleanRequestUrlParser;
+require_once("components/CleanRequestUrlParser.php");
+
+abstract class BaseModuleController
+{
+    protected $explodedPathToModule;
+
+    /** @param array $explodedPathToModule */
+    public function construct($explodedPathToModule) {
+        $this->explodedPathToModule = $explodedPathToModule;
+        $this->parseRequestVars(count($this->explodedPathToModule));
+        $this->run();
+    }
+
+    /** @param int $offset */
+    protected function parseRequestVars($offset) {
+        if ($_SERVER["REQUEST_METHOD"] === "GET") {
+            CleanRequestUrlParser::instance()->parseGetVars($offset);
+        } else { // assume it is a "POST"
+            CleanRequestUrlParser::instance()->parsePostVars();
+        }
+    }
+
+    abstract function run();
+}
