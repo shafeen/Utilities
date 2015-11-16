1bf6a7a1d92f3abc27da3f64fedfec02d4ee95cb
Shafeen M <sodl02@gmail.com>
2015-11-16 13:52:29 -0500

    ModuleView base class will now encapsulate dependency information. This way every single child View will not need to create its own variables to specify what the dependency paths (like jquery, css files) are.

--- PHP/mvc/views/TestView.php
+++ PHP/mvc/views/TestView.php
@@ -8,15 +8,8 @@ require_once("views/ModuleView.php");
 class TestView extends ModuleView {
 
     protected function extractInfoFromModel(&$model) {
-        // parse the url for the module to run
-        // expected path format = {serverroot}/app/{module
-        $this->reqPathParamArray = $model["reqPathParamArray"];
-
-        // need to define the location for our used components
-        $this->jQueryPath = "bower_components/jquery/dist/jquery.min.js";
-        $this->bootstrapCssPath = "bower_components/bootstrap/dist/css/bootstrap.min.css";
-        $this->bootstrapOptionalThemeCssPath = "bower_components/bootstrap/dist/css/bootstrap-theme.min.css";
-        $this->bootstrapJavascriptPath = "bower_components/bootstrap/dist/js/bootstrap.min.js";
+        // set the variables needed for the main html file
+        $this->reqPathParamArray = $model->getReqPathParamArray();
     }
 
 }
