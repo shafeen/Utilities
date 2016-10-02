8b9969cf3c358610a5aa275d461bb24d6a871899
Shafeen M <sodl02@gmail.com>
2016-10-01 23:49:52 -0400

    testing a usecase for java's PriorityQueue with a custom class

--- Java/DataStructures/src/main/java/Main.java
+++ Java/DataStructures/src/main/java/Main.java
@@ -1,6 +1,50 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class Main {
 
+    public static class Person implements Comparable {
+        final String name;
+
+        public int getPriority() {
+            return priority;
+        }
+
+        final int priority;
+
+        public Person(String name, int priority) {
+            this.name = name;
+            this.priority = priority;
+        }
+
+
+        @Override
+        public int compareTo(Object o) {
+            if (this.priority > ((Person)o).getPriority()) {
+                return 1;
+            } else if (this.priority == ((Person) o).getPriority()) {
+                return 0;
+            } else {
+                return -1;
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
+        // testTrie();
+
+        // Testing use of a PriorityQueue
+        Queue<Person> priorityQueue = new PriorityQueue<>();
+        priorityQueue.add(new Person("Employee", 2));
+        priorityQueue.add(new Person("Boss", 1));
+
+        while (priorityQueue.size() > 0) {
+            System.out.println(priorityQueue.remove().name);
+        }
+    }
+
+    private static void testTrie() {
         Trie radixTree = new Trie();
 
         radixTree.addWord("samsonite");
